c99125d3896fba681f5ae4c3d602aee6d65833d8
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-04-11 12:14:20 -0300

    a

--- app/src/main/java/com/betrybe/trybevirtualmenu/views/MenuItemDetailActivity.kt
+++ app/src/main/java/com/betrybe/trybevirtualmenu/views/MenuItemDetailActivity.kt
@@ -9,13 +9,13 @@ import com.betrybe.trybevirtualmenu.R
 import com.betrybe.trybevirtualmenu.models.MenuDatabase
 
 class MenuItemDetailActivity : AppCompatActivity() {
-
-    private val image: ImageView by lazy { findViewById(R.id.detail_image) }
-    private val name: TextView by lazy { findViewById(R.id.detail_name) }
-    private val description: TextView by lazy { findViewById(R.id.detail_description) }
-    private val price: TextView by lazy { findViewById(R.id.detail_price) }
-
-    private val back: Button by lazy { findViewById(R.id.detail_back) }
+//
+//    private val image: ImageView by lazy { findViewById(R.id.detail_image) }
+//    private val name: TextView by lazy { findViewById(R.id.detail_name) }
+//    private val description: TextView by lazy { findViewById(R.id.detail_description) }
+//    private val price: TextView by lazy { findViewById(R.id.detail_price) }
+//
+//    private val back: Button by lazy { findViewById(R.id.detail_back) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +24,13 @@ class MenuItemDetailActivity : AppCompatActivity() {
         val menuId = intent.getIntExtra("menu_id", 0)
         val getMenuId = MenuDatabase.getMenuById(menuId + 1)!!
 
-        name.text = getMenuId.name
-        image.setImageResource(getMenuId.image)
-        description.text = getMenuId.description
-        price.text = getMenuId.price
-
-        back.setOnClickListener{
-            finish()
-        }
+//        name.text = getMenuId.name
+//        image.setImageResource(getMenuId.image)
+//        description.text = getMenuId.description
+//        price.text = getMenuId.price
+//
+//        back.setOnClickListener{
+//            finish()
+//        }
     }
 }
